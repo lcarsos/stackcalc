4d7e64c9d5f1519ef940b142934b901cdd5ad063
lcarsos <lcarsos@lcarsos.com>
2020-08-10 21:15:35 -0600

    oh, that's done it. good to know.
    It's weird that you can instantiate structs with full access to its
    innards in new() just because you're in the impl.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use stackcalc::stack::Stack;
 
 fn main() {
-    let mut s: Stack<f32>;
+    let mut s: Stack<f32> = Stack::new();
     s.push(3.0);
     s.push(2.0);
     println!("end of stack: {}", s.peek().unwrap());
+    s.pop();
+    println!("new end of stack: {}", s.pop().unwrap());
 }
--- src/stack.rs
+++ src/stack.rs
@@ -5,6 +5,12 @@ pub struct Stack<T: Copy> {
 }
 
 impl<T: Copy> Stack<T> {
+    pub fn new() -> Stack<T> {
+        Stack {
+            vec: Vec::new()
+        }
+    }
+
     pub fn push(&mut self, x: T) {
         self.vec.push(x);
     }
